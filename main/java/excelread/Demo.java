f677244abb1e1e10915ac096e7e5bcaf816b88c4
Subin Sunny chacko <subinsunnychacko@Subins-MacBook-Air.local>
2022-04-27 18:57:40 -0700

    New

---
+++ main/java/excelread/Demo.java
@@ -0,0 +1,96 @@
+
+class Demo 
+{
+
+	public static void main(String[] args) 
+	{
+		int a,b,sum;
+		a=10;
+		b=20;
+		sum =a+b;
+		System.out.print("The sum is "+sum);
+	}
+
+}
+
+
+class Demo2 
+{
+
+	public static void main(String[] args) 
+	{
+		int a = 10;
+		float b=20.02f;
+		float sum;
+		sum =a+b;
+		System.out.print("The sum is "+sum);
+	}
+
+}
+
+
+class Demo3
+{
+
+     public static void main(String args[])
+     {
+        System.out.println("My name is Subin");
+        System.out.println("My age is 26");
+     }
+} 
+
+
+class Demo4
+{
+
+     public static void main(String args[])
+     {
+        float base,height,area;
+        base=10;
+        height=20;
+        area=(base*height)/2;
+        System.out.print("The area is "+area);
+        
+     }
+} 
+
+
+class Demo5
+{
+
+     public static void main(String args[])
+     {
+        int x,y,temp;
+        x=10;
+        y=20;
+        System.out.println("The value of x before swaping is "+x+" and y before swaping is "+y);
+        temp=x;
+        x=y;
+        y=temp;
+        System.out.println("The value of x after swaping is "+x+" and y after swaping is "+y);      
+     }
+} 
+
+
+class Demo6
+{
+
+     public static void main(String args[])
+     {
+        int x,y;
+        x=10;
+        y=20;
+        System.out.println("The value of x before swaping is "+x+" and y before swaping is "+y);
+        x=x+y;			//value of x becomes 30
+        y=x-y;			//value of y becomes 10
+        x=x-y;			//value of x becomes 20 
+        System.out.println("The value of x after swaping is "+x+" and y after swaping is "+y);       
+     }
+} 
+
+
+
+
+
+
+
